8404a3af72a20f41d1e34f32c4a7b94b1e487bf6
13each <amschelkov@gmail.com>
2026-04-27 00:38:40 +0300

    Basic auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
+import 'core/api_client.dart';
+import 'screens/auth/auth_stub_screen.dart';
+
+const _authStubsEnabled = bool.fromEnvironment('AUTH_STUBS', defaultValue: false);
 
 void main() {
+  if (_authStubsEnabled) {
+    runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: AuthStubScreen(apiClient: ApiClient()),
+      ),
+    );
+    return;
+  }
   runApp(const HabicalApp());
 }
