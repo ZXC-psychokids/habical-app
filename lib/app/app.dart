f2f4a8eb2b929bf1f3dc71df2c77be246804d785
13each <amschelkov@gmail.com>
2026-04-27 20:54:38 +0300

    app: wire auth, friends, calendar, and settings repositories

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -5,54 +5,72 @@ import '../core/api_client.dart';
 import '../repositories/api_friends_repository.dart';
 import '../repositories/api_habits_repository.dart';
 import '../repositories/api_home_repository.dart';
+import '../repositories/auth_repository.dart';
+import '../repositories/calendar_repository.dart';
 import '../repositories/friends_repository.dart';
 import '../repositories/habits_repository.dart';
 import '../repositories/home_repository.dart';
-import '../repositories/hybrid_friends_repository.dart';
 import '../repositories/hybrid_habits_repository.dart';
 import '../repositories/hybrid_home_repository.dart';
 import '../repositories/in_memory_app_store.dart';
-import '../screens/root_screen.dart';
+import '../repositories/settings_repository.dart';
+import '../screens/auth/auth_gate_screen.dart';
+import '../services/session_service.dart';
 
 class HabicalApp extends StatefulWidget {
-  const HabicalApp({super.key});
+  const HabicalApp({
+    super.key,
+    required this.apiClient,
+    required this.sessionService,
+  });
+
+  final ApiClient apiClient;
+  final SessionService sessionService;
 
   @override
   State<HabicalApp> createState() => _HabicalAppState();
 }
 
 class _HabicalAppState extends State<HabicalApp> {
-  late final InMemoryAppStore store;
-  late final ApiClient apiClient;
+  late final InMemoryAppStore _store;
 
   @override
   void initState() {
     super.initState();
-    store = InMemoryAppStore();
-    apiClient = ApiClient();
+    _store = InMemoryAppStore();
   }
 
   @override
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
       providers: [
-        RepositoryProvider<ApiClient>.value(value: apiClient),
+        RepositoryProvider<ApiClient>.value(value: widget.apiClient),
+        RepositoryProvider<SessionService>.value(value: widget.sessionService),
+        RepositoryProvider<AuthRepository>(
+          create: (_) => AuthRepository(
+            apiClient: widget.apiClient,
+            sessionService: widget.sessionService,
+          ),
+        ),
+        RepositoryProvider<SettingsRepository>(
+          create: (_) => SettingsRepository(apiClient: widget.apiClient),
+        ),
+        RepositoryProvider<CalendarRepository>(
+          create: (_) => ApiCalendarRepository(apiClient: widget.apiClient),
+        ),
         RepositoryProvider<HabitsRepository>(
           create: (_) => HybridHabitsRepository(
-            remoteRepository: ApiHabitsRepository(apiClient: apiClient),
-            store: store,
+            remoteRepository: ApiHabitsRepository(apiClient: widget.apiClient),
+            store: _store,
           ),
         ),
         RepositoryProvider<FriendsRepository>(
-          create: (_) => HybridFriendsRepository(
-            remoteRepository: ApiFriendsRepository(apiClient: apiClient),
-            store: store,
-          ),
+          create: (_) => ApiFriendsRepository(apiClient: widget.apiClient),
         ),
         RepositoryProvider<HomeRepository>(
           create: (_) => HybridHomeRepository(
-            remoteRepository: ApiHomeRepository(apiClient: apiClient),
-            store: store,
+            remoteRepository: ApiHomeRepository(apiClient: widget.apiClient),
+            store: _store,
           ),
         ),
       ],
@@ -64,7 +82,12 @@ class _HabicalAppState extends State<HabicalApp> {
           scaffoldBackgroundColor: const Color(0xFFF2F2F2),
           fontFamily: 'Cera Pro',
         ),
-        home: const RootScreen(),
+        home: Builder(
+          builder: (context) => AuthGateScreen(
+            authRepository: context.read<AuthRepository>(),
+            sessionService: widget.sessionService,
+          ),
+        ),
       ),
     );
   }
